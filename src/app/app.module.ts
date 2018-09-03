63adf3f4239e79ad7f849dc954646cd0b0a770d9
yaronmil2@gmail.com <yaronmil2@gmail.com>
2018-09-03 11:32:37 +0300

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
